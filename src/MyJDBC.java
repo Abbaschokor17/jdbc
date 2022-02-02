c54e4201639cd4a2ec5451a0db8334e331776362
Abbas chokor <Abbaschokor@hotmail.com>
2022-02-02 22:10:11 +0200

    initial commit

---
+++ src/MyJDBC.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class MyJDBC {
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "abbas1722000chokor?");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from people");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("firstname"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
